2e7789065f24f04d6215867d3cd411c9c4121dee
Prácticas Tec. de la Información 2 <ptecnologias2@ASMEDIA.MX>
2024-01-08 16:39:48 -0600

    avance en asmedia

---
+++ resources/views/register.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Bienvenido</h1>
+    <form action="/register" method="post">
+        <label>Nombre
+            <input type="text" name="name" id="" placeholder="Nombre">
+        </label>
+        <label>Email
+            <input type="email" name="email" placeholder="E-mail">
+        </label>
+        <label>Contraseña 
+            <input type="password" name="password" placeholder="Constraseña">
+        </label>
+        
+    </form>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +15,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    if(auth()->check()){
+        return view('dashboard');
+    }
+    
+    return view('index');
 });
+
+Route::get('/login', function(){
+    return view('login');
+});
+
+Route::get('/register', function(){
+    return view('register');
+});
+
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/logout', [UserController::class, 'logout']);
